2db09d2dd6e1a8d0e38a6938edf25e43273a2099
Pierre-Loan Janczak <loan.janczak@gmail.com>
2017-05-01 15:23:34 +0200

    Update BinaryHeap.java

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -13,15 +13,6 @@ public class BinaryHeap {
 		heap = new int [capacity+1]; // index 0 not used
 		next = 0;
 	}
-	public void exch(int a, int b) {
-		int temp = heap[a];
-		heap[a] = heap[b];
-		heap[b] = temp;
-	}
-	public int getMaxChild(int a, int b) {
-		if (heap[a] > heap[b]) return a;
-		else return b;
-	}
 	public void insert(int val) {
 		heap[++next] = val;
 		swim(next);
@@ -34,7 +25,16 @@ public class BinaryHeap {
 		next--;
 		return val;
 	}
-	public void swim(int k) {
+	private void exch(int a, int b) {
+		int temp = heap[a];
+		heap[a] = heap[b];
+		heap[b] = temp;
+	}
+	private int getMaxChild(int a, int b) {
+		if (heap[a] > heap[b]) return a;
+		else return b;
+	}
+	private void swim(int k) {
 		while(k > 1) {
 			if (heap[k] > heap[k/2]){
 				exch(k, k/2);
@@ -43,7 +43,7 @@ public class BinaryHeap {
 			else break;
 		}
 	}
-	public void sink(int k) {
+	private void sink(int k) {
 		int i = 2*k;
 		while(i+1 < next && (heap[k] < heap[i] || heap[k] < heap[i+1])) {
 			exch(k, getMaxChild(i, i+1));
