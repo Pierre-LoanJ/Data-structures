52f96ce0b65ebfef3992cccd77809dccfcbecfd3
Pierre-Loan Janczak <loan.janczak@gmail.com>
2017-09-11 15:42:44 +0200

    using generics

--- sort/Mergesort.java
+++ sort/Mergesort.java
@@ -1,29 +1,42 @@
 package sort;
-public class Mergesort {
-  public static void merge(char t[], char temp[], int lo, int mi, int hi) {
+public class Mergesort<T extends Comparable<T>> {
+  public void merge(T t[], T temp[], int lo, int mi, int hi) {
     for (int k = lo; k <= hi; k++) {
       temp[k] = t[k];
     }
     int i = lo, j = mi+1;
     for (int k = lo; k <= hi; k++) {
-      if (i > mi)                 t[k] = temp[j++];
-      else if (j > hi)            t[k] = temp[i++];
-      else if (temp[i] > temp[j]) t[k] = temp[j++];
-      else                        t[k] = temp[i++];
+      if (i > mi)                 				t[k] = temp[j++];
+      else if (j > hi)            				t[k] = temp[i++];
+      else if (temp[i].compareTo(temp[j]) > 0) 	t[k] = temp[j++];
+      else                        				t[k] = temp[i++];
     }
   }
-  public static void divide(char t[], char temp[], int lo, int hi) {
+  public void divide(T t[], T temp[], int lo, int hi) {
+    if (hi <= lo) return;
+    
     int mi = lo + (hi - lo) / 2;
-    if (hi <= lo) { return; }
-    else {
-      divide(t, temp, lo, mi);
-      divide(t, temp, mi, hi);
-      merge(t, temp, lo, mi, hi);
-    }
+    divide(t, temp, lo, mi);
+    divide(t, temp, mi+1, hi);
+    merge(t, temp, lo, mi, hi);
   }
-  public static void sort(char t[]) {
-    char[] temp = new char[t.length];
+  public void sort(T t[]) {
+    @SuppressWarnings("unchecked")
+	T[] temp = (T[]) new Comparable[t.length];
     divide(t, temp, 0, t.length - 1);
+  }
+  public void display (T[] a) {
+	for (int i = 0; i < a.length; i++) {
+		System.out.print(a[i] + "  -  ");
+	}
+	System.out.println("");
+  }
+  public static void main(String[] args) {
+	  Integer[] a = { 4, 3, 1, 6, 2, 5 };
+	  Mergesort<Integer> ms = new Mergesort<Integer>();
+	  ms.display(a);
+	  ms.sort(a);
+	  ms.display(a);
   }
 }
 
